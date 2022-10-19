4aea74be3dab949f2d55ed17d78310b5e2f1190f
Jessie <hire.jesslw@gmail.com>
2022-10-19 09:49:52 -1000

    build function to take player turns

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
-let state = ["X", "", "O", "", "X", "O", "", "", ""];
+const square = document.querySelectorAll('.square');
+square.forEach(i => 
+        i.onclick = (e) => gameFlow.play(i));
 
-
-
+//model - needs its own CRUD
 const gameboard = (() => {
-    const display = () => {
+ //gameboard/state needs to be updated, methods to update state 
+    let state =  ["", "", "", "", "", "", "", "", ""];
+    return {state};
+})();
+
+//view - also needs its own CRUD
+const displayController = (() => {
+    const display = (state) => {
         for(let i=0; i<= (state.length - 1); i++) {
         document.getElementById([i]).innerText = state[i];
     }
@@ -11,32 +19,46 @@ const gameboard = (() => {
     return {display};
 })();
 
-const gameFlowModule = (() => {
+//controller - control state changes, player turn is a game state, check for wins
+const gameFlow = (() => {
+    let player1turn = true;
+    const play = (i) => {
+           if (player1turn == true) {
+            i.innerText = "X";
+            gameboard.state[i.id] = "X";
+            console.log(gameboard.state);
+            console.log(i);
+            checkForWin();
+            return player1turn = false;
+           } else {
+            i.innerText = "O";
+            gameboard.state[i.id] = "O";
+            console.log(gameboard.state);
+            console.log(i);
+            checkForWin();
+            return player1turn = true;
+           }
+    };
+    const checkForWin = () => {
+
+        }
+
 
     
 
-    return {};
+    return {play, checkForWin};
 })();
 
 const playerFactory = (name) => {
-    const square = document.querySelectorAll('.square');
     const getName = () => name;
-    const addMark = (i) => {
-        square.forEach(i => 
-        i.onclick = (e) => {
-        if (i.innerText == "") {
-            i.innerText = getName();
-            };
-            //update state???
-        });
-    };
-
-    return {getName, addMark};
+    return {getName};
 };
 
 const player1 = playerFactory('X');
 const player2 = playerFactory('O');
 
-gameboard.display();
-player2.addMark();
+displayController.display(gameboard.state);
+
+
+
 
