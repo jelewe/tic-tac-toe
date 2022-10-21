89ed94b219780b8f534c6e6faeb2549f12b93bfd
Jessie <hire.jesslw@gmail.com>
2022-10-21 12:31:35 -1000

    create random computer player and add start screen with player button selectors

--- index.js
+++ index.js
@@ -1,10 +1,6 @@
-const square = document.querySelectorAll('.square');
-square.forEach(i => 
-        i.onclick = (e) => gameFlow.play(i));
 
-//model - needs its own CRUD
+//model 
 const gameboard = (() => {
- //gameboard/state needs to be updated, methods to update state 
     let state =  [null, null, null, null, null, null, null, null, null];
     const reset = (state) => {
         state = [null, null, null, null, null, null, null, null, null];
@@ -13,13 +9,14 @@ const gameboard = (() => {
     return {state, reset};
 })();
 
-//view - also needs its own CRUD
+//view 
 const displayController = (() => {
     const display = (state) => {
         for(let i=0; i<= (state.length - 1); i++) {
         document.getElementById([i]).innerText = state[i];
         };
     };
+
     const displayWinner = (player1turn) => {
         const div = document.querySelector('#winnerDisplay');
         if (player1turn == true) {
@@ -32,15 +29,19 @@ const displayController = (() => {
             div.innerText = "";
         };
     };
+
     const resetDiv = () => {
         const div = document.querySelector('#winnerDisplay');
         div.innerText = "";
     };
+
     return {display, displayWinner, resetDiv};
 })();
 
-//controller - control state changes, player turn is a game state, check for wins
+//controller
 const gameFlow = (() => {
+    const startMenu = document.querySelector('.start');
+    const square = document.querySelectorAll('.square');
     let player1turn = true;
     const play = (i) => {
            if (player1turn == true) {
@@ -62,6 +63,32 @@ const gameFlow = (() => {
             return player1turn = true;
            };
     };
+
+    const playComputer = (i) => {
+        if (player1turn == true) {
+            if (gameboard.state[i.id] != null) {
+                return;
+            };
+            i.innerText = "X";
+            gameboard.state[i.id] = "X";
+            checkForWin();
+            player1turn = false;
+           };
+           computerMove();
+    };
+
+    const computerMove = () => {
+        computerChoice = square[Math.floor(Math.random()*square.length)];
+        if (gameboard.state[computerChoice.id] != null) {
+            computerMove();
+        } else {
+            computerChoice.innerText = "O";
+            gameboard.state[computerChoice.id] = "O";
+            checkForWin();
+            return player1turn = true;
+        };
+    };
+
     const checkForWin = () => {
         if (gameboard.state[0] != null) {
             if (gameboard.state[0] == gameboard.state[1] && gameboard.state[0] == gameboard.state[2] ||
@@ -69,7 +96,6 @@ const gameFlow = (() => {
                 gameboard.state[0] == gameboard.state[4] && gameboard.state[0] == gameboard.state[8]) {
                     displayController.displayWinner(player1turn);
                    gameFlow.stopPlay();
-                   return;
                 };
             };
         if (gameboard.state[2] != null) {
@@ -77,28 +103,24 @@ const gameFlow = (() => {
                     gameboard.state[2] == gameboard.state[4] && gameboard.state[2] == gameboard.state[6]) {
                         displayController.displayWinner(player1turn);
                         gameFlow.stopPlay();
-                        return;
                     };
                 };
         if (gameboard.state[1] != null) {
             if (gameboard.state[1] == gameboard.state[4] && gameboard.state[1] == gameboard.state[7]) {
                 displayController.displayWinner(player1turn);
                 gameFlow.stopPlay();
-                return;
             };
         };
         if (gameboard.state[3] != null ) {
             if (gameboard.state[3] == gameboard.state[4] && gameboard.state[3] == gameboard.state[5]) {
                 displayController.displayWinner(player1turn);
                 gameFlow.stopPlay();
-                return;
             };
         };
         if (gameboard.state[6] != null) {
             if (gameboard.state[6] == gameboard.state[7] && gameboard.state[6] == gameboard.state[8]) {
                 displayController.displayWinner(player1turn);
                 gameFlow.stopPlay();
-                return;
             };
         };
         if (gameboard.state[0] != null && 
@@ -122,14 +144,48 @@ const gameFlow = (() => {
     const resetGame = () => {
         gameboard.state = [null, null, null, null, null, null, null, null, null];
         displayController.display(gameboard.state);
-        square.forEach(i => 
-            i.onclick = (e) => gameFlow.play(i));
         displayController.resetDiv();
+        startMenu.style.display = '';
+        gameFlow.start();
         return player1turn = true;
     };
-    
 
-    return {play, checkForWin, stopPlay, resetGame};
+    const start = () => {
+        startMenu.style.position = 'absolute';
+        startMenu.style.height= '100%';
+        startMenu.style.width= '100%';
+        startMenu.innerText= (`
+        
+        Let's play tic-tac-toe!
+        
+        Choose gameplay:
+        
+        `);
+        let button1 = document.createElement('button');
+        button1.addEventListener('click', start2Player);
+        button1.classList = 'startBtn';
+        button1.innerText = "2 players";
+        startMenu.appendChild(button1);
+        let button2 = document.createElement('button');
+        button2.addEventListener('click', startComputer);
+        button2.classList = 'startBtn';
+        button2.innerText = "Computer";
+        startMenu.appendChild(button2);
+    };
+
+    const start2Player = () => {
+        square.forEach(i => 
+        i.onclick = (e) => gameFlow.play(i));
+        startMenu.style.display = 'none';
+    };
+
+    const startComputer = () => {
+        square.forEach(i => 
+            i.onclick = (e) => gameFlow.playComputer(i));
+        startMenu.style.display = 'none';
+    };
+
+    return {play, playComputer, checkForWin, stopPlay, resetGame, start};
 })();
 
 const playerFactory = (name) => {
@@ -140,7 +196,7 @@ const playerFactory = (name) => {
 const player1 = playerFactory('X');
 const player2 = playerFactory('O');
 
-displayController.display(gameboard.state);
+gameFlow.start();
 
 
 
